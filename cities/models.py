4da6a8a13de14d64914a232ab1f63d456b1768ff
Denis Solovev <denissolovev99@icloud.com>
2021-10-19 19:32:27 +0300

    cities, add, create, delete, update, paginate

---
+++ cities/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+# Create your models here.
+from django.urls import reverse_lazy
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название города')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+        ordering = ['name']
+
+    def get_absolute_url(self):
+        return reverse_lazy('cities:detail', kwargs={'pk': self.pk})
+
